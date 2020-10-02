8c7c80ffd396984cbaaa5f7c7bf96448fe0b1138
davner <davner@email.arizona.edu>
2020-10-02 09:49:35 -0700

    more logging to get info

--- vatt_secondary.cpp
+++ vatt_secondary.cpp
@@ -944,7 +944,6 @@ void Secondary::TimerHit()
 		//update the position
 		if(!isMoving)
 		{
-      printf("%lf", NextPos.pos);
 			deepcopy(CorrNextPos, NextPos);
 			correct(CorrNextPos, el, temp);
 			for(int ii=0; ii<6; ii++)
