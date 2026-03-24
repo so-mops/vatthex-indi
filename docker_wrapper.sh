d065773d4739c9994c0c87a07a799ff9a3bba711
Omar Navarro <omardanielnavarro@gmail.com>
2026-03-24 15:06:13 -0700

    set the data directory to match previous implementation

--- docker_wrapper.sh
+++ docker_wrapper.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-DATA_DIR="${DATA_DIR:-$SCRIPT_DIR/vatthex-data}"
+DATA_DIR="/home/vattobs/.mtnops"
 
 mkdir -p "$DATA_DIR"
 
